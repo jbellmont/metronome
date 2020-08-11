f6bd126eb5f3b0d4478cfe0e4bcc0bbb62914365
jbellmont <bellmont@gmail.com>
2020-08-11 22:04:59 +0100

    Added styling (hovers on buttons) and when the play button is active. Also disabled the play button once the metronome is playing, to ensure it can't be started over and over again. Added a volume slider, but need to ensure it does reset every time the sample is changed

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-let sample = document.querySelector('.synth-sample');
-let intervalID; // Used to clear the interval - i.e. pause the metronome
+// Sets the sample + default sample
+let sample = document.querySelector('.synth-sample'); 
+
+// Sets the default volume
+sample.volume = 0.5; // Sets default volume
+
+// Used to clear the interval - i.e. pause the metronome
+let intervalID; 
+
+// Play button click-event
+const playButton = document.getElementById('play-button');
+playButton.addEventListener('click', togglePlay);
+
+// Pause button click-event
+const pauseButton = document.getElementById('pause-button');
+pauseButton.addEventListener('click', pauseSample);
+
+// Change sample button click-event
+const sampleButton = document.getElementById('sample-button');
+sampleButton.addEventListener('click', changeSample);
+
+// Change volume range change-event
+const volumeSlider = document.getElementById('volume-slider');
+volumeSlider.addEventListener('change', changeVolume);
+
+
+//--------------------//
+// app functionality // 
+//------------------//
 
 function playSample () {
   // Plays the sample, and is triggered every interval
@@ -10,11 +37,23 @@ function togglePlay() {
   // Reads the tempo value from the input form and triggers sample
   let tempo = 60000 / document.getElementById('tempo-field').value;
   intervalID = setInterval(playSample, tempo);
+
+  // Changes appearance of active play button
+  playButton.className = 'button-active';
+
+  //disables play button while sample is playing
+  playButton.setAttribute("disabled", "true"); 
 }
 
 function pauseSample() {
   // Pauses sample, resetting the interval function
   clearInterval(intervalID);
+
+  // Enables the play button again
+  playButton.removeAttribute("disabled");
+
+  // Resets the class for the play button
+  playButton.className = 'button';
 }
 
 function changeSample() {
@@ -26,14 +65,7 @@ function changeSample() {
   }
 }
 
-// Play button
-const playButton = document.getElementById('play-button');
-playButton.addEventListener('click', togglePlay);
-
-// Pause button
-const pauseButton = document.getElementById('pause-button');
-pauseButton.addEventListener('click', pauseSample);
-
-// Change sample button
-const sampleButton = document.getElementById('sample-button');
-sampleButton.addEventListener('click', changeSample);
+function changeVolume() {
+  // Changes the volume value
+  sample.volume = this.value / 100; // Sets default volume
+}
